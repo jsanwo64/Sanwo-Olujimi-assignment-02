aa8017e4f640bbdaa957f391540ac54425612f81
Olujimi Sanwo <jsanwo64@gmail.com>
2022-07-09 20:13:11 +0000

    attempted challenge 1 and challenge 2

--- src/assignment.js
+++ src/assignment.js
@@ -1,3 +1,4 @@
+
 // ========================
 // DO NOT EDIT THIS BLOCK
 const assignment = {};
@@ -14,9 +15,15 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+
+let sum = 0;
+
+for (i = 0; i < arrayOfNumbers.length; i++) {
+  sum += arrayOfNumbers[i];
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+  return sum;
+}
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +35,15 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let even = 0;
+  for (i = 0; i < arrayOfNumbers.length; i++) {
+            if (arrayOfNumbers[i] % 2 === 0) {
+                even++
+            }
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+  return even;
+}
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
